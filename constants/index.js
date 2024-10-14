d84c29d4ad62232214dffcd7cc25e60d5924dc52
marnel <TiomarOne02@gmail.com>
2024-10-14 11:58:21 +0800

    .

--- constants/index.js
+++ constants/index.js
@@ -4,7 +4,11 @@ export const navLinks = [
 		url: "/",
 		icon: "/icons/gauge-solid.svg",
 	},
-
+	{
+		label: "Consumables",
+		url: "/consumables",
+		icon: "/icons/handshake-angle-solid.svg",
+	},
 	{
 		label: "Borrow",
 		url: "/borrow",
