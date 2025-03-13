a6455420310b675d18e4ccae75ac5a0614a6fbaf
Romeo <110727246+Romeo-Jr@users.noreply.github.com>
2025-03-14 07:29:17 +0800

    Update ActivityTable.jsx

--- components/activity/ActivityTable.jsx
+++ components/activity/ActivityTable.jsx
@@ -12,17 +12,18 @@ const ActivityTable = ({ activities }) => {
 					<tr className="bg-gray-100">
 						<th className="p-4 text-left border">Action</th>
 						<th className="p-4 text-left border">User</th>
-						<th className="p-4 text-left border">Description</th>
 						<th className="p-4 text-left border">Name</th>
+						<th className="p-4 text-left border">Description</th>
 						<th className="p-4 text-left border">Timestamp</th>
 					</tr>
 				</thead>
 				<tbody>
 					{activities.map((activity, index) => (
 						<tr key={index} className="border-b hover:bg-gray-50">
-							<td className="p-4 border">{activity.action}</td>
+							<td className="p-4 border">{activity.activity}</td>
 							<td className="p-4 border">{activity?.User.name}</td>
-							<td className="p-4 border">{activity.description}</td>
+							<td className="p-4 border">{activity.name}</td>
+							<td className="p-4 border">{activity.desc}</td>
 							<td className="p-4 border">
 								{new Date(activity.createdAt).toLocaleString()}
 							</td>
